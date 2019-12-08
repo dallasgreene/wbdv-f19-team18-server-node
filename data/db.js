7c295efbaf31224f1203e0f0394dbbebb1dc0605
dallasgreene <rkdaproxy@gmail.com>
2019-12-08 18:59:34 -0500

    connection to the mongo database

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
 module.exports = () => {
     const mongoose = require('mongoose');
     const dbName = 'wbdv-f19-team18-db-mongo';
-    let connectionString = 'mongodb://localhost/';
+    let connectionString = 'mongodb://localhost:27017/';
     connectionString += dbName;
-    
+
     mongoose.connect(connectionString);
 };
