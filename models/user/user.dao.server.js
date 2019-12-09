f68b5f63dd91696f4511d38fb980048c1ce3bd8d
dallasgreene <rkdaproxy@gmail.com>
2019-12-09 01:00:52 -0500

    refactored to be cleaner

--- models/user/user.dao.server.js
+++ models/user/user.dao.server.js
@@ -2,47 +2,50 @@ const userModel = require('./user.model.server');
 const recipeDAO = require('../recipe/recipe.dao.server');
 const commentDAO = require('../comment/comment.dao.server');
 
+const fetchReferencedObjects = user => {
+    return userModel.find({ _id: { $in: user.followers } }, '_id username firstName lastName')
+        .then(followers => {
+
+    return userModel.find({ _id: { $in: user.following } }, '_id username firstName lastName')
+        .then(following => {
+
+    return recipeDAO.findAllRecipesIn(user.likedRecipes)
+        .then(likedRecipes => {
+
+    return commentDAO.findAllCommentsIn(user.comments)
+        .then(comments => {
+            return {
+                _id: user._id,
+                firstName: (user.firstName) ? user.firstName : "",
+                lastName: (user.lastName) ? user.lastName : "",
+                username: (user.username) ? user.username : "",
+                password: (user.password) ? user.password : "",
+                followers: followers,
+                following: following,
+                email: (user.email) ? user.email : [],
+                diets: (user.diets) ? user.diets : [],
+                likedRecipes: likedRecipes,
+                comments: comments,
+                __v: user.__v
+            };
+        });
+        });
+        });
+        });
+};
+
 const findAllUsers = () => {
-    return userModel.find();
+    return userModel.find({ }, "_id firstName lastName username");
 };
 
 const findUserById = userId => {
     return userModel.findById(userId)
-        .then(user => {
-            return userModel.find({ _id: { $in: user.followers } }, '_id username firstName lastName')
-                .then(followers => {
-
-            return userModel.find({ _id: { $in: user.following } }, '_id username firstName lastName')
-                .then(following => {
-
-            return recipeDAO.findAllRecipesIn(user.likedRecipes)
-                .then(likedRecipes => {
-
-            return commentDAO.findAllCommentsIn(user.comments)
-                .then(comments => {
-                    return {
-                        _id: user._id,
-                        firstName: (user.firstName) ? user.firstName : "",
-                        lastName: (user.lastName) ? user.lastName : "",
-                        username: (user.username) ? user.username : "",
-                        password: (user.password) ? user.password : "",
-                        followers: followers,
-                        following: following,
-                        email: (user.email) ? user.email : [],
-                        diets: (user.diets) ? user.diets : [],
-                        likedRecipes: likedRecipes,
-                        comments: comments,
-                        __v: user.__v
-                    };
-                });
-                });
-                });
-                });
-        });
+        .then(user => fetchReferencedObjects(user));
 };
 
 const findUserByUsername = username => {
-    return userModel.findOne({ username: username });
+    return userModel.findOne({ username: username })
+        .then(user => fetchReferencedObjects(user));
 };
 
 const createUser = user => {
