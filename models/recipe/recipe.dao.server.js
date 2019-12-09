510bdbaaa8f02c429606966529ee7b400efc29ca
dallasgreene <rkdaproxy@gmail.com>
2019-12-08 21:58:35 -0500

    implemented recipe CRUD ops

--- models/recipe/recipe.dao.server.js
+++ models/recipe/recipe.dao.server.js
@@ -2,4 +2,34 @@ const mongoose = require('mongoose');
 const recipeSchema = require('./recipe.schema.server');
 const recipeModel = mongoose.model('RecipeModel', recipeSchema);
 
-module.exports = recipeModel;
+const findAllRecipes = () => {
+    return recipeModel.find();
+};
+
+const findRecipeById = recipeId => {
+    return recipeModel.findById(recipeId);
+};
+
+const createRecipe = recipe => {
+    return recipeModel.create(recipe);
+};
+
+const updateRecipe = (recipeId, recipe) => {
+    return recipeModel.updateOne({ _id: recipeId }, { $set: recipe })
+        .then(() => findRecipeById(recipeId))
+        .catch(() => { return { status: "incorrect recipe id" } });
+};
+
+const deleteRecipe = recipeId => {
+    return recipeModel.deleteOne({ _id: recipeId })
+        .then(() => findAllRecipes())
+        .catch(() => { return { status: "incorrect recipe id" } });
+};
+
+module.exports = {
+    findAllRecipes,
+    findRecipeById,
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
+};
