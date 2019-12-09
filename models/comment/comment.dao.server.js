fa33f0b2f18baf0950c29119ccc88071ba096bae
dallasgreene <rkdaproxy@gmail.com>
2019-12-08 21:47:25 -0500

    implemented comment CRUD ops

--- models/comment/comment.dao.server.js
+++ models/comment/comment.dao.server.js
@@ -2,4 +2,35 @@ const mongoose = require('mongoose');
 const commentSchema = require('./comment.schema.server');
 const commentModel = mongoose.model('CommentModel', commentSchema);
 
-module.exports = commentModel;
+const findAllComments = () => {
+    return commentModel.find();
+};
+
+const findCommentById = commentId => {
+    return commentModel.findById(commentId);
+};
+
+const createComment = comment => {
+    return commentModel.create(comment);
+};
+
+const updateComment = (commentId, comment) => {
+    return commentModel.updateOne({ _id: commentId }, { $set: comment })
+        .then(() => findCommentById(commentId))
+        .catch(() => { return { status: "incorrect comment id" } });
+};
+
+const deleteComment = commentId => {
+    return commentModel.deleteOne({ _id: commentId })
+        .then(() => findAllComments())
+        .catch(() => { return { status: "incorrect comment id" } });
+};
+
+
+module.exports = {
+    findAllComments,
+    findCommentById,
+    createComment,
+    updateComment,
+    deleteComment
+};
