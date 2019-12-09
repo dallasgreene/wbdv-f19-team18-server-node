06577fce4b2cb7723fbc6939b10ba3b8e43e1fe0
dallasgreene <rkdaproxy@gmail.com>
2019-12-08 21:45:35 -0500

    implemented admin CRUD ops

--- models/admin/admin.dao.server.js
+++ models/admin/admin.dao.server.js
@@ -1,5 +1,40 @@
 const adminSchema = require('./admin.schema.server');
-const userModel = require('../user/user.dao.server');
+const userModel = require('../user/user.model.server');
 const adminModel = userModel.discriminator('AdminModel', adminSchema);
 
-module.exports = adminModel;
+const findAllAdmins = () => {
+    return adminModel.find();
+};
+
+const findAdminById = adminId => {
+    return adminModel.findById(adminId);
+};
+
+const findAdminByUsername = username => {
+    return adminModel.findOne({ username: username });
+};
+
+const createAdmin = admin => {
+    return adminModel.create(admin);
+};
+
+const updateAdmin = (adminId, admin) => {
+    return adminModel.updateOne({ _id: adminId }, { $set: admin })
+        .then(() => findAdminById(adminId))
+        .catch(() => { return { status: "incorrect admin id" } });
+};
+
+const deleteAdmin = adminId => {
+    return adminModel.deleteOne({ _id: adminId })
+        .then(() => findAllAdmins())
+        .catch(() => { return { status: "incorrect admin id" } });
+};
+
+module.exports = {
+    findAllAdmins,
+    findAdminById,
+    findAdminByUsername,
+    createAdmin,
+    updateAdmin,
+    deleteAdmin
+};
