509bf9bf94bcea6c8f0ca06d4b05b542116a5855
dallasgreene <rkdaproxy@gmail.com>
2019-12-09 18:18:35 -0500

    adding new database for heroku remote server

--- data/db.js
+++ data/db.js
@@ -4,5 +4,5 @@ module.exports = () => {
     let connectionString = 'mongodb://localhost:27017/';
     connectionString += dbName;
 
-    mongoose.connect(connectionString);
+    mongoose.connect('mongodb://heroku_4dwgh6wb:aj8jihr203uglpp7ivr8f5f581@ds353358.mlab.com:53358/heroku_4dwgh6wb');
 };
