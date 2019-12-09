967e7900916ab07611f64ccc54bfe8ccc3f21d9c
dallasgreene <rkdaproxy@gmail.com>
2019-12-08 20:35:50 -0500

    started CRUD for user

--- models/user/user.dao.server.js
+++ models/user/user.dao.server.js
@@ -2,4 +2,12 @@ const mongoose = require('mongoose');
 const userSchema = require('./user.schema.server');
 const userModel = mongoose.model('UserModel', userSchema);
 
+const findAllUsers = () => {
+    return userModel.find();
+};
+
+const findUserById = userId => {
+    return userModel.find({ _id: userId });
+};
+
 module.exports = userModel;
